12f27d2f743fd73b4289e512063280eb6d96c3d3
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2026-03-02 12:15:58 +0100

    nästan klar med klasserna

--- omprovtest2/SuperVillain.cs
+++ omprovtest2/SuperVillain.cs
@@ -1,47 +1,66 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace omprovtest2;
 
 public class SuperVillain
 {
-    public string Name; 
-    public int TheorySkill; 
-    public int DrivingSkill; 
-    public int Patience; 
-    public bool HasLicence; 
+    public string Name;
+    public int TheorySkill;
+    public int DrivingSkill;
+    public int Patience;
+    public bool HasLicence;
 
     public void LearnTheory()
     {
 
-        int spel=Random.Shared.Next(2); 
-        if (Patience==0)
+        int spel = Random.Shared.Next(2);
+        if (Patience == 0)
         {
             Console.WriteLine("Du får inget körkort");
         }
-        else if (spel==1)
+        else if (spel == 1)
         {
             Console.WriteLine("Du lyckades");
-            TheorySkill +=5; 
+            TheorySkill += 5;
         }
-        else  
+        else if (spel == 2)
         {
             Console.WriteLine("Du misslyckades");
-            Patience--; 
+            Patience -= 1;
         }
-        // ingen patience kvar? misslyckas!
-        // lyckas? theoryskill++
-        // misslyckas? patience--
+
     }
     public void LearnDriving()
     {
-        int pal=Random.Shared.Next(2); 
-        if (Patience==0)
+        int pal = Random.Shared.Next(2);
+        if (Patience == 0)
         {
-            
+            Console.WriteLine("Du får inget körkort");
+        }
+        else if (pal == 1)
+        {
+            Console.WriteLine("Du lyckades");
+            DrivingSkill += 5;
+        }
+        else if (pal == 2)
+        {
+            Console.WriteLine("Du misslyckades");
+            Patience -= 1;
         }
     }
     public bool TakeTest()
     {
-        return HasLicence=true; 
+        int play = Random.Shared.Next(TheorySkill);
+        int steer = Random.Shared.Next(DrivingSkill);
+        if (play > 40 && steer > 40)
+        {
+
+        }
+        else
+        {
+
+        }
+
     }
 }
